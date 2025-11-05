c353a67c215b3f2d4e6f48c73fb43169f6b184c3
Qyzylxan <hitriybomber@mail.ru>
2025-11-06 00:43:25 +0300

    redis conn

---
+++ auth/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: "",
+		DB:       0,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		log.Fatal("[auth]: REDIS connection failed - ", err)
+	}
+	log.Println("[auth]: REDIS initialized")
+
+}
