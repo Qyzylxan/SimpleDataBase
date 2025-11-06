f971b04e499db223c087a4ef4de4773fde57c65a
Qyzylxan <hitriybomber@mail.ru>
2025-11-07 01:49:19 +0300

    a

--- auth/main.go
+++ auth/main.go
@@ -18,9 +18,11 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	if _, err := rdb.Ping(ctx).Result(); err != nil {
 		log.Fatal("[auth]: REDIS connection failed - ", err)
+	} else {
+		log.Println("[auth]: REDIS initialized")
 	}
-	log.Println("[auth]: REDIS initialized")
 
 }
